bf3db27e23d59a79fdd7d491a65086e8e53a6a17
AhmedNoaman602 <ahmednoaman002@gmail.com>
2024-08-23 15:10:02 +0300

    creating a form for adding jobs

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Employer;
+use App\Models\Post;
+use App\Models\Tag;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Post>
+ */
+class PostFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'title' => $this->faker->title(),
+            'tag_id' => Tag::factory(),
+            'body' => $this->faker->paragraph(3 , true),
+        ];
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -2,31 +2,45 @@
 
 use App\Models\Customer_Reviews;
 use Illuminate\Support\Facades\Route;
-use App\Models\Job;
+
 use App\Models\Post;
 use App\Models\Tag;
-
+use App\Models\Job;
 
 Route::get('/', function () {
      return view('home');
 });
 
 Route::get('/jobs', function () {
-    $jobs = Job::with('employer')->get();
-    return view('jobs' , [
+    $jobs = Job::with('employer')->cursorPaginate(3);
+
+    return view('jobs.index' , [
         'jobs' => $jobs
     ]);
 });
+Route::get('/jobs/create' , function (){
+    return view('jobs.create');
+});
+Route::get('/jobs/{id}', function ($id) {
+    $job = Job::find($id);
+    return view('jobs.show' , ['job' => $job]);
+});
+
+Route::post('/jobs', function () {
+    Job::create([
+        'title' => request('title'),
+        'salary' => request('salary'),
+        'employer_id' => 1,
+    ]);
+    return redirect('/jobs');
+});
+
+
 Route::get('/contact', function () {
     return view('contact');
 });
 
-Route::get('/jobs/{id}', function ($id) {
-  $job = Job::find($id);
 
-return view('job' , ['job' => $job]);
-
-});
 
 Route::get('/customer_reviews', function () {
     return view('customer_reviews' , [
